5cf8c65e369dbdc1091df91fdbeaf9bf4947f3fb
Nhan Nguyen <hoainhaannguyen@gmail.com>
2026-03-05 16:03:42 +0700

    Initial commit

--- angular-playground/src/app/app.config.ts
+++ angular-playground/src/app/app.config.ts
@@ -1,15 +1,30 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {
+  createInterceptorCondition,
+  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+  IncludeBearerTokenCondition,
+  includeBearerTokenInterceptor,
+  provideKeycloak,
+} from 'keycloak-angular';
 import { routes } from './app.routes';
-
-import { provideKeycloak } from 'keycloak-angular';
 import { keycloakConfig } from './keycloak.config';
+
+const urlCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
+  urlPattern: /^(http:\/\/localhost:3000)(\/.*)?$/i,
+  bearerPrefix: 'Bearer',
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideKeycloak(keycloakConfig),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    {
+      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+      useValue: [urlCondition], // <-- Note that multiple conditions might be added.
+    },
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
   ],
 };
