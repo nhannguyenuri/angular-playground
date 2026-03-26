fa46751b975a80a7a934ab2673a10f82f9f7bf0b
Nhan Nguyen <hoainhaannguyen@gmail.com>
2026-03-26 10:08:17 +0700

    Initial commit

--- angular-playground/src/app/app.spec.ts
+++ angular-playground/src/app/app.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import Keycloak from 'keycloak-js';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        {
+          provide: Keycloak,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
   });
 
